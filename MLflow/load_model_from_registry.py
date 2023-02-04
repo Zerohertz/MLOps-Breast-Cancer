d94bcbb12d34d7ec60aab321643d67db118fcc38
Zerohertz <ohg3417@gmail.com>
2023-02-05 01:26:26 +0900

    :boom: Add: Model Registry

---
+++ MLflow/load_model_from_registry.py
@@ -0,0 +1,33 @@
+import os
+from argparse import ArgumentParser
+
+import mlflow
+import pandas as pd
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+
+os.environ["MLFLOW_S3_ENDPOINT_URL"] = "http://localhost:9000"
+os.environ["MLFLOW_TRACKING_URI"] = "http://localhost:5001"
+os.environ["AWS_ACCESS_KEY_ID"] = "zerohertz_minio"
+os.environ["AWS_SECRET_ACCESS_KEY"] = "asdf456!"
+
+parser = ArgumentParser()
+parser.add_argument("--model-name", dest="model_name", type=str, default="sk_model")
+parser.add_argument("--run-id", dest="run_id", type=str)
+args = parser.parse_args()
+
+model_pipeline = mlflow.sklearn.load_model(f"runs:/{args.run_id}/{args.model_name}")
+
+df = pd.read_csv("../Etc./DB.csv")
+
+X, y = df.drop(["id", "timestamp", "target"], axis="columns"), df["target"]
+X_train, X_valid, y_train, y_valid = train_test_split(X, y, train_size=0.8, random_state=2023)
+
+train_pred = model_pipeline.predict(X_train)
+valid_pred = model_pipeline.predict(X_valid)
+
+train_acc = accuracy_score(y_true=y_train, y_pred=train_pred)
+valid_acc = accuracy_score(y_true=y_valid, y_pred=valid_pred)
+
+print("Train Accuracy :", train_acc)
+print("Valid Accuracy :", valid_acc)
---
+++ MLflow/save_model_to_registry.py
@@ -0,0 +1,60 @@
+import os
+from argparse import ArgumentParser
+
+import mlflow
+import pandas as pd
+import psycopg2
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVC
+
+os.environ["MLFLOW_S3_ENDPOINT_URL"] = "http://localhost:9000"
+os.environ["MLFLOW_TRACKING_URI"] = "http://localhost:5001"
+os.environ["AWS_ACCESS_KEY_ID"] = "zerohertz_minio"
+os.environ["AWS_SECRET_ACCESS_KEY"] = "asdf456!"
+
+db_connect = psycopg2.connect(
+    user="zerohertz",
+    password="qwer123!",
+    host="localhost",
+    port="1234",
+    database="Breast_Cancer",
+)
+df = pd.read_sql("SELECT * FROM breast_cancer_data ORDER BY id DESC LIMIT 100", db_connect)
+
+X, y = df.drop(["id", "timestamp", "target"], axis="columns"), df["target"]
+X_train, X_valid, y_train, y_valid = train_test_split(X, y, train_size=0.8, random_state=2023)
+
+model_pipeline = Pipeline([("scaler", StandardScaler()), ("svc", SVC())])
+model_pipeline.fit(X_train, y_train)
+
+train_pred = model_pipeline.predict(X_train)
+valid_pred = model_pipeline.predict(X_valid)
+
+train_acc = accuracy_score(y_true=y_train, y_pred=train_pred)
+valid_acc = accuracy_score(y_true=y_valid, y_pred=valid_pred)
+
+print("Train Accuracy :", train_acc)
+print("Valid Accuracy :", valid_acc)
+
+parser = ArgumentParser()
+parser.add_argument("--model-name", dest="model_name", type=str, default="sk_model")
+args = parser.parse_args()
+
+mlflow.set_experiment("new-exp")
+
+signature = mlflow.models.signature.infer_signature(model_input=X_train, model_output=train_pred)
+input_sample = X_train.iloc[:10]
+
+with mlflow.start_run():
+    mlflow.log_metrics({"train_acc": train_acc, "valid_acc": valid_acc})
+    mlflow.sklearn.log_model(
+        sk_model=model_pipeline,
+        artifact_path=args.model_name,
+        signature=signature,
+        input_example=input_sample,
+    )
+
+df.to_csv("../Etc./DB.csv", index=False)
