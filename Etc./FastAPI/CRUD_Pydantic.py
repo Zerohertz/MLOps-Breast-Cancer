bc70778aed0235659e71ca369fbeaa18c55e5fbd
Zerohertz <ohg3417@gmail.com>
2023-02-12 14:48:32 +0900

    :hammer: Update: FastAPI Examples

---
+++ Etc./FastAPI/CRUD_Pydantic.py
@@ -0,0 +1,42 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+
+class CreateIn(BaseModel):
+    name: str
+    nickname: str
+
+class CreateOut(BaseModel):
+    status: str
+    id: int
+
+app = FastAPI()
+USER_DB = {}
+NAME_NOT_FOUND = HTTPException(status_code=400, detail="Name not found.")
+
+@app.post("/users", response_model=CreateOut)
+def create_user(user: CreateIn) -> CreateOut:
+    USER_DB[user.name] = user.nickname
+    user_dict = user.dict()
+    user_dict["status"] = "success"
+    user_dict["id"] = len(USER_DB)
+    return user_dict
+
+@app.get("/users")
+def read_user(name: str):
+    if name not in USER_DB:
+        raise NAME_NOT_FOUND
+    return {"nickname": USER_DB[name]}
+
+@app.put("/users")
+def update_user(name: str, nickname: str):
+    if name not in USER_DB:
+        raise NAME_NOT_FOUND
+    USER_DB[name] = nickname
+    return {"status": "success"}
+
+@app.delete("/users")
+def delete_user(name: str):
+    if name not in USER_DB:
+        raise NAME_NOT_FOUND
+    del USER_DB[name]
+    return {"status": "success"}
